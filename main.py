fd28eaeedcfb137e67b1e69c89dd02969851cbd1
AttoCat <wazasiyo2525a@gmail.com>
2021-02-28 21:02:24 +0900

    :hammer: Edit error-handling and minor fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from base64 import b64encode
 from os import getenv
+from sys import exit
 from time import sleep
 
 from dotenv import load_dotenv
@@ -14,26 +15,22 @@ DISCORD_WEBHOOK_URL = getenv("DISCORD_WEBHOOK_URL")
 
 
 class SpotifyToDiscord:
-    def get_playlist_items(self):
+    def get_playlist(self):
         header = {"Authorization": f"Bearer {self.token}"}
         params = {
             "market": "JP",
             "fields": "tracks.items(added_by(href),track(name,external_urls,artists(name),id))",
         }
-        items = get(
+        playlist = get(
             f"https://api.spotify.com/v1/playlists/{PLAYLIST_ID}",
             headers=header,
             params=params,
         )
-        return items
-
-    def get_user_name(self, href):
-        header = {"Authorization": f"Bearer {self.token}"}
-        user_name = get(href, headers=header).json()["display_name"]
-        return user_name
+        return playlist
 
     def set_new_token(self):
-        encoded_code = b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode()).decode()
+        encoded_code = b64encode(
+            f"{CLIENT_ID}:{CLIENT_SECRET}".encode()).decode()
         header = {"Authorization": f"Basic {encoded_code}"}
         param = {"grant_type": "client_credentials"}
         self.token = post(
@@ -47,16 +44,6 @@ class SpotifyToDiscord:
                 additions.append(item)
         return additions
 
-    def combine_additions(self, additions):
-        for addition in additions:
-            track_name = addition["track"]["name"]
-            track_spotify_url = addition["track"]["external_urls"]["spotify"]
-            track_artist_name = " & ".join(
-                [artist["name"] for artist in addition["track"]["artists"]]
-            )
-            added_user_name = self.get_user_name(addition["added_by"]["href"])
-            yield (track_name, track_artist_name, track_spotify_url, added_user_name)
-
     def extraction_deletions(self, items):
         deletions = []
         for now_item in self.now_items:
@@ -66,20 +53,26 @@ class SpotifyToDiscord:
 
     @staticmethod
     def error_handling(exception):
-        post(DISCORD_WEBHOOK_URL, json={"content": f"内部エラーが発生しました:{exception}"})
+        post(
+            DISCORD_WEBHOOK_URL, json={
+                "content": f"内部エラーが発生しました。\n```{exception}```\nシステムを終了します。"}
+        )
+        exit()
 
     def start(self):
         self.set_new_token()
-        self.now_items = self.get_playlist_items().json()["tracks"]["items"]
+        self.now_items = self.get_playlist().json()["tracks"]["items"]
         while True:
-            items = self.get_playlist_items().json()["tracks"]["items"]
+            playlist = self.get_playlist().json()
+            items = playlist["items"]["tracks"]["items"]
             additions = self.extraction_additions(items)
             if additions:
-                for combined_addition in self.combine_additions(additions):
+                for combined_addition in self.combine_additions_or_deletions(additions):
                     print(combined_addition)
             deletions = self.extraction_deletions(items)
             if deletions:
-                print(deletions)
+                for combined_deletion in self.combine_additions_or_deletions(deletions):
+                    print(combined_deletion)
             self.now_items = items
             sleep(1)
 
