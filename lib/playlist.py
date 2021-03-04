dfb62b0b7c1b0baabf26aa023293c6ef1f3fee8b
AttoCat <wazasiyo2525a@gmail.com>
2021-03-04 18:37:38 +0900

    :fire: Delete playlist-object

--- lib/playlist.py
+++
@@ -1,7 +0,0 @@
-class Playlist:
-    def __init__(self, playlist_data):
-        self.name = playlist_data["name"]
-        self.url = playlist_data["external_urls"]["spotify"]
-        self.image = playlist_data["images"][0]["url"]
-        self.items = playlist_data["tracks"]["items"]
-        self.total_followers = playlist_data["followers"]["total"]
