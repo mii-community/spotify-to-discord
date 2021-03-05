ce1c6f57d8160af8976f9c42087fd8d6b742d350
AttoCat <wazasiyo2525a@gmail.com>
2021-03-05 18:28:12 +0900

    :hammer: Minor fix

--- lib/addition_or_deletion.py
+++ lib/addition_or_deletion.py
@@ -45,7 +45,7 @@ class Deletion:
             "spotify"
         ]
         self.album_image = track_detail["album"]["images"][0]["url"]
-        self.artist_name = "&".join(
+        self.artist_name = ", ".join(
             [
                 artist["name"]
                 for artist in track_detail["artists"]
